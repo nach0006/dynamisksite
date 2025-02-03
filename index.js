3552470de2c582e3ac94948017c771f9a16af7cd
nach0006 <nach0006@stud.kea.dk>
2025-02-03 14:49:45 +0100

    url categories

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ let listContainer = document.querySelector(".category_list_container");
 
 fetch(`https://kea-alt-del.dk/t7/api/categories/`) // sender en GET-anmodning (forespørgsel til serveren)
   .then((response) => response.json()) // konverterer responsen til JSON (JavaScriipt objekt)
-  .then((data) => showList(data)); //henter data
+  .then((data) => showCategory(data)); //henter data
 
-function showList(categories) {
+function showCategory(categories) {
   //bruger bestemt data (categories)
-  console.log(categories);
+  console.log("mine data er: ", categories);
   let markup = categories
     .map(
       (category) =>
         //for each category... and copy a category´s html and change the data (in img - link to the images link!!!)
-        `<a class="category grow" href="productlist.html">
+        `<a class="category grow" href="productlist.html?category=${category.category}">
                     <h3>${category.category}</h3>
                 </a>`
     )
@@ -19,3 +19,13 @@ function showList(categories) {
   console.log(markup);
   listContainer.innerHTML = markup;
 }
+
+// URL
+//opret et URLSearchParams-objekt og hent værdien af "color" (blue)
+const myCategory = new URLSearchParams(window.location.search).get("category");
+
+//viser "color: blue" i consolen
+console.log("Category:", myCategory);
+
+//viser værdien af farven som h2
+document.querySelector("h3").textContent = `${myCategory}`;
